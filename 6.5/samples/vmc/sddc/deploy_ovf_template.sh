48e0a8258c09ab0b345ea8182253ce53e977c99c
Tianhao He <het@vmware.com>
2018-06-12 14:42:53 +0800

    add a 6.5 folder

---
+++ 6.5/samples/vmc/sddc/deploy_ovf_template.sh
@@ -0,0 +1,3 @@
+export PYTHONPATH=/Users/het/github/vsphere-automation-sdk-python/
+export VC=vcenter.sddc-50-112-61-134.vmc.vmware.com
+python /Users/het/github/vsphere-automation-sdk-python/samples/vmc/sddc/deploy_ovf_template.py -s $VC -u 'cloudadmin@vmc.local' -p '0ln*%e3Wxd' -c --libitemname 'ovf_template_1'
